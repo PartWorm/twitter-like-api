75f1946cddfc568ce1380eb655714ae7a87f9615
PartWorm <partworm@gmail.com>
2021-05-13 21:59:01 +0900

    Cleanup read_children_or_thread

--- html/read_children_or_thread.php
+++ html/read_children_or_thread.php
@@ -3,13 +3,10 @@
 include_once 'get_important_child.php';
 
 function read_children_or_thread($conn, &$post, $order_by, $depth = 1) {
-	if ($depth >= 4) {
+	if ($depth >= 5) {
 		return;
 	}
-	if ($post['n_children'] == 0) {
-		return;
-	}
-	if ($depth < 3) {
+	if ($depth < 4) {
 		$children_result = get_stmt_result($conn, <<<SQL
 			SELECT id, author, content, timestamp, n_children, n_descendants
 			FROM posts
@@ -22,23 +19,22 @@ function read_children_or_thread($conn, &$post, $order_by, $depth = 1) {
 		$children = array();
 		while ($child = mysqli_fetch_assoc($children_result)) {
 			$child['timestamp'] = to_relative_time(strtotime($child['timestamp']));
-			read_children_or_thread($conn, $child, $order_by, $depth + 1);
+			read_children_or_thread($conn, $child, 'timestamp ASC', $depth + 1);
 			$children[] = $child;
 		}
 		$post['children'] = $children;
 	}
 	else {
 		$thread = array();
-		$child = get_important_child($conn, $post['id'], $order_by);
-		if (is_null($child)) {
-			http_response_code(404);
-			die();
-		}
-		do {
-			$thread[] = $child;
+		$child = $post;
+		while (true) {
 			$child = get_important_child($conn, $child['id'], $order_by);
+			if (is_null($child)) {
+				break;
+			}
+			$child['timestamp'] = to_relative_time(strtotime($child['timestamp']));
+			$thread[] = $child;
 		}
-		while (!is_null($child));
 		$post['thread'] = $thread;
 	}
 }
